bd46cb25b3688753ebdbf40a44f9e8ca477ea9fc
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:22:01 +0100

    Initial commit

--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,18 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13.0"
 
-  required_providers {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    sdm = {
+      source  = "strongdm/sdm"
+      version = ">= 1.0.19"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.2.0"
+    }
+  }
 }
